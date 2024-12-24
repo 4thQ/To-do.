b9096cdfbc9053d7c500e8e15a69d7194fcd8258
4thQ <164560954+4thQ@users.noreply.github.com>
2024-12-24 18:12:43 +0000

    updated index with css & added logic

--- todo.js
+++ todo.js
@@ -3,6 +3,10 @@ const div = document.querySelector(".content");
 const addTaskBtns = document.querySelectorAll(".add-task-btn");
 const addTaskBtn = document.querySelector(".add-task-btn");
 const closeBtn = document.querySelector(".close-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const taskForm = document.querySelector(".task-form");
+const addTask = document.querySelector(".add-task");
+const h3 = document.querySelector("h3");
 
 addTaskBtns.forEach((button) => {
   button.addEventListener("click", () => {
@@ -14,4 +18,35 @@ closeBtn.addEventListener("click", () => {
   dialog.close();
 });
 
-addTaskBtn.click();
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let form = new FormData(taskForm);
+
+  let taskTitle = form.get("task-title");
+  let taskPriority = form.get("task-priority");
+  let date = form.get("schedule");
+  let notes = form.get("notes");
+
+  const li = document.createElement("li");
+  li.innerHTML = taskTitle;
+  addTask.appendChild(li);
+  li.addEventListener("click", (e) => {
+    li.style.textDecoration == "line-through"
+      ? (li.style.textDecoration = "")
+      : (li.style.textDecoration = "line-through");
+  });
+
+  const taskDelete = document.createElement("button");
+
+  taskDelete.classList.add("task-delete");
+  taskDelete.innerText = "delete";
+  taskDelete.addEventListener("click", (e) => {
+    console.log("hey");
+    taskDelete.closest("li").remove();
+  });
+
+  li.appendChild(taskDelete);
+
+  dialog.close();
+});
